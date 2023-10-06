542a4ea3b4d7f06ac4d51c87df6a0974273ab735
Imper1um <github@imper1umdesigns.com>
2023-10-06 10:36:19 -0400

    Fixes 32
    This repairs the functionality for macro execution and generation.

--- src/ceus.js
+++ src/ceus.js
@@ -1,11 +1,15 @@
-import { ceus_RequestWindow } from "./ceus_RequestWindow.js";
+import { ceus_LogEngine } from "./ceus_LogEngine.js";
 import { ceus_PlayerRequestWindow } from "./ceus_PlayerRequestWindow.js";
 import { ceus_ProviderEngine } from "./ceus_ProviderEngine.js";
-import { ceus_SocketEngine } from "./ceus_SocketEngine.js";
-import { ceus_SettingsEngine } from "./ceus_SettingsEngine.js";
+import { ceus_RequestActor } from "./ceus_RequestActor.js";
+import { ceus_RequestItem } from "./ceus_RequestItem.js";
+import { ceus_RequestOptions } from "./ceus_RequestOptions.js";
+import { ceus_RequestUser } from "./ceus_RequestUser.js";
+import { ceus_RequestWindow } from "./ceus_RequestWindow.js";
 import { ceus_ResultsWindow } from "./ceus_ResultsWindow.js";
-import { ceus_LogEngine } from "./ceus_LogEngine.js";
-
+import { ceus_SettingsEngine } from "./ceus_SettingsEngine.js";
+import { ceus_SocketEngine } from "./ceus_SocketEngine.js";
+import { Utils } from "./Utils.js";
 
 export class Ceus {
 	constructor() {
@@ -15,6 +19,8 @@ export class Ceus {
 		this.providerEngine = new ceus_ProviderEngine();
 		this.settingsEngine = new ceus_SettingsEngine();
 		this.socketEngine = new ceus_SocketEngine();
+		
+		globalThis.Ceus = this;
 	}
 	
 	static current = new Ceus();
@@ -171,23 +177,111 @@ export class Ceus {
 		});
 	}
 	
-	static gmRoll(data) {
+	expandMacroData(data) {
+		const requestOptions = new ceus_RequestOptions(
+			data.title,
+			data.message,
+			Utils.uuidv4(),
+			Utils.uuidv4(),
+			data.rollNumber,
+			data.rollPrivacy,
+			data.rollRequirePrivacy
+		);
+		const requestItems = [];
+		const requestActors = [];
+		const requestUsers = [];
+		const requestWindow = new ceus_RequestWindow();
+		const rp = Ceus.current.providerEngine.currentRollProvider;
+		const actors = game.actors.entities || game.actors.contents;
+		const users = game.users.entities || game.users.contents;
+		for (const requestItem of data.requestItems) {
+			const newRi = new ceus_RequestItem(
+				requestItem.rollId,
+				requestItem.trainedOption,
+				requestItem.dc,
+				requestItem.customBonus,
+				requestItem.advantageDisadvantage,
+				requestItem.canCritSuccess,
+				requestItem.canCritFail
+			);
+			const possibleAction = requestWindow.possibleActions.find(pa => pa.id == requestItem.rollId);
+			if (!possibleAction) { continue; }
+			newRi.rollType = possibleAction.rollType;
+			newRi.trainedOptions = rp.rollTrainedOptions(possibleAction.rollType, possibleAction.rollId);
+			newRi.allowDC = rp.allowDC(possibleAction.rollType, possibleAction.rollId);
+			newRi.permitDC = rp.permitDC();
+			newRi.permitAdvantageDisadvantage = rp.permitAdvantageDisadvantage();
+			requestItems.push(newRi);
+		}
+		
+		for (const requestActor of data.requestActors) {
+			const actor = actors.find(a => a._id === requestActor.actorId);
+			const possibleActor = requestWindow.possibleActors.find(pa => pa.actorId === requestActor.actorId);
+			if (!actor || !possibleActor) { continue; }
+
+			const newRa = new ceus_RequestActor(
+				requestActor.actorId,
+				actor.name,
+				possibleActor.imgUrl
+			);
+			requestActors.push(newRa);
+		}
+		
+		for (const requestUser of data.requestUsers) {
+			const user = users.find(u => u._id === requestUser.userId);
+			const possibleUser = requestWindow.possibleUsers.find(pu => pu.id === requestUser.userId);
+			if (!user || !possibleUser) { continue; }
+			
+			const newRu = new ceus_RequestUser(
+				requestUser.userId,
+				user.name,
+				possibleUser.img
+			);
+			requestUsers.push(newRu);
+		}
+		requestOptions.requestActors = requestActors;
+		requestOptions.requestItems = requestItems;
+		requestOptions.requestUsers = requestUsers;
+		
+		Ceus.log.Debug("expandMacroData:requestOptions", requestOptions);
+		
+		return requestOptions;
+	}
+	
+	async macro_gmRoll(data) {
 		if (!game.user.isGM) { return; }
+		Ceus.log.Debug("macro_gmRoll", data);
+		
+		data = this.expandMacroData(data);
 		
 		const resultsWindow = new ceus_ResultsWindow(data, "gmroll");
-		resultsWindow.render(true);
-		resultsWindow.roll(data.requestActors.map(a => a.actorId));
+		resultsWindow.handleRequestOptions();
+		await resultsWindow.render(true);
+		await resultsWindow.roll(data.requestActors.map(a => a.actorId));
 	}
 	
-	static openRequest(data) {
+	async macro_openRequest(data) {
 		if (!game.user.isGM) { return; }
+		Ceus.log.Debug("macro_openRequest", data);
+		
+		data = this.expandMacroData(data);
 		
 		const requestWindow = new ceus_RequestWindow();
-			this.requestWindows.push(requestwindow);
-			requestwindow.render(true);
+		requestWindow.fill(data);
+		this.requestWindows.push(requestWindow);
+		await requestWindow.render(true);
 	}
 	
-	static requestRoll(data) {
+	async macro_requestRollWindow(data) {
 		if (!game.user.isGM) { return; }
+		Ceus.log.Debug("macro_requestRollWindow", data);
+		
+		data = this.expandMacroData(data);
+		
+		const resultsWindow = new ceus_ResultsWindow(data, "ask");
+		resultsWindow.handleRequestOptions();
+		await resultsWindow.render(true);
+		await resultsWindow.notify(data.requestActors.map(a => a.actorId));
+		
 	}
 }
